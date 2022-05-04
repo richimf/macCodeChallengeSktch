8583dc51d1a46ad446b22ee18e137ca050a6a139
Richie <richimf1@gmail.com>
2022-05-03 19:25:20 -0500

    Project creation, first build success

---
+++ SketchFigures/SketchFigures/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SketchFigures
+//
+//  Created by Ricardo Montesinos on 03/05/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SketchFigures/SketchFigures/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SketchFigures
+//
+//  Created by Ricardo Montesinos on 03/05/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
