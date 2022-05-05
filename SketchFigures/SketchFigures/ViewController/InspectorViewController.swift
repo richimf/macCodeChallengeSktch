6e9ee0e2640cad5785c871b5d7a41a8a226e45b9
Richie <richimf1@gmail.com>
2022-05-05 17:31:52 -0500

    Clean code

--- SketchFigures/SketchFigures/ViewController/InspectorViewController.swift
+++ SketchFigures/SketchFigures/ViewController/InspectorViewController.swift
@@ -1,3 +1,52 @@
-        //        guard let splitViewController = self.parent as? NSSplitViewController,
-        //              let canvasVC = splitViewController.splitViewItems[1].viewController as? ViewController else { return }
-        //        canvasVC.updateFromInspector()
+//
+//  InspectorViewController.swift
+//  SketchFigures
+//
+//  Created by Ricardo Montesinos on 05/05/22.
+//
+
+import Cocoa
+
+final class InspectorViewController: NSViewController {
+
+    @IBOutlet private weak var positionX: NSTextField!
+    @IBOutlet private weak var positionY: NSTextField!
+    @IBOutlet private weak var width: NSTextField!
+    @IBOutlet private weak var height: NSTextField!
+    @IBOutlet private weak var colorWell: NSColorWell!
+    
+    var data: Shape?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func update(_ sender: Any) {
+        print(#function)
+        updateCanvas()
+    }
+    
+    func updateCanvas() {
+        guard let parent = self.parent, let updatedShape = data else { return }
+        let entityManager = ShapeEntityManager(parent: parent)
+        entityManager.updateCanvasView(with: updatedShape)
+    }
+    
+    func updateInspectorData() {
+        if let originX = data?.frame.origin.x {
+            self.positionX.stringValue = originX.format()
+        }
+        if let originY = data?.frame.origin.y {
+            self.positionY.stringValue = originY.format()
+        }
+        if let width = data?.frame.width {
+            self.width.stringValue =  width.format()
+        }
+        if let height = data?.frame.height {
+            self.height.stringValue =  height.format() 
+        }
+        if let color = data?.color {
+            colorWell.color = NSColor(cgColor: color) ?? .clear
+        }
+    }
+}
--- SketchFigures/SketchFigures/ViewController/ViewController.swift
+++ SketchFigures/SketchFigures/ViewController/ViewController.swift
@@ -1,17 +1,82 @@
-//    func updateInspector(data: Shape) {
-//        guard let splitViewController = self.parent as? NSSplitViewController,
-//              let inspectoVC = self.storyboard?.instantiateController(withIdentifier: "inspectorID") as? InspectorViewController
-//                      else { return }
-//        //let item = NSSplitViewItem(viewController: inspectoVC)
-//        guard let ivc = splitViewController.presentedViewControllers?.first as? InspectorViewController else { return }
-//        //var items = splitViewController.splitViewItems
-//        //items[1] = item
-//        //splitViewController.splitViewItems = items
-//        ivc.data = data
-//        ivc.updateInspectorData()
-//    }
-//extension ViewController: DataSelectedDelegate {
-//    func selected(shape: Shape) {
-//        <#code#>
-//    }
-//}
+//
+//  ViewController.swift
+//  SketchFigures
+//
+//  Created by Ricardo Montesinos on 03/05/22.
+//
+
+import Cocoa
+
+final class ViewController: NSViewController {
+    
+    @IBOutlet private weak var canvas: CanvasContainer!
+    
+    private let viewModel = ViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        canvas.delegate = self
+        canvas.needsDisplay = true
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor = NSColor.lightGray.cgColor
+        self.view.addSubview(canvas)
+    }
+
+    override var representedObject: Any? {
+        didSet {
+         }
+    }
+
+    private func createShapeOn(position: CGPoint) {
+        // TODO: MOVE THIS TO THE VIEWMODEL
+        let shapeView = viewModel.build(at: position)
+        shapeView.setPanGesture(target: self, delegate: self, selector: #selector(handlePan(_:)))
+        shapeView.setClickGesture(target: self, delegate: self, selector: #selector(handleClick(_:)))
+        self.view.updateLayer()
+        self.canvas.addSubview(shapeView)
+    }
+
+    // MARK: - GESTURE SELECTORS
+    @objc private func handlePan(_ gesture: NSPanGestureRecognizer) {
+        guard let shapeView = gesture.view as? ShapeView else { return }
+        let distance = gesture.translation(in: shapeView)
+        let newCenter = CGPoint(x: shapeView.frame.origin.x + distance.x, y: shapeView.frame.origin.y + distance.y)
+        shapeView.frame.origin = newCenter
+        shapeView.shape.frame = shapeView.frame
+        gesture.setTranslation(CGPoint.zero, in: shapeView)
+        // Update Inspector
+        updateInspectorAtt(from: shapeView)
+    }
+    
+    @objc private func handleClick(_ gesture: NSClickGestureRecognizer) {
+        guard let shapeView = gesture.view as? ShapeView else { return }
+        // Update Inspector
+        updateInspectorAtt(from: shapeView)
+    }
+    
+    private func updateInspectorAtt(from shapeView: ShapeView) {
+        guard let parent = self.parent else { return }
+        let entityManager = ShapeEntityManager(parent: parent)
+        entityManager.updateInspectorView(with: shapeView.shape)
+    }
+    
+    func updateFromInspector() {
+        print("update selected shape")
+    }
+}
+// MARK: - MOUSE EVENTS
+extension ViewController: CanvasListener {
+    func onMouseClick(position: CGPoint) {
+        createShapeOn(position: position)
+    }
+}
+// MARK: - GESTURES
+extension ViewController: NSGestureRecognizerDelegate {
+  func gestureRecognizer(_ gestureRecognizer: NSGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: NSGestureRecognizer) -> Bool {
+    return true
+  }
+
+  func gestureRecognizer(_ gestureRecognizer: NSGestureRecognizer, shouldAttemptToRecognizeWith event: NSEvent) -> Bool {
+    return true
+  }
+}
